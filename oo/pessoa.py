7b88d67056bcfd941dd067993d411043330de155
DiksonSantos <dikson_santos@outlook.com>
2020-09-14 00:41:31 -0300

    Criado_Metodo_Cumprimentar

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,4 +1,21 @@
 class Pessoa:
-    pass
+
+    def greets(this):
+        return f"Oi {id(this)}"
+
+if __name__=='__main__':
+    p = Pessoa()
+    print(Pessoa.greets(p))
+    #Metodo Usual:
+    print(p.greets())
+
+    print(id(p))
+
+    #Obs:
+    #print(p.greets(7))
+    #Seria o mesmo que:
+    #print(Pessoa.greets(p,7))
+    #Isto a Cima seria o mesmo que estar passando 2 argumentos
+    #..a FUN só suporta 1.
 
 #Alteration
